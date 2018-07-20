94ec345d0ee72a54fe70c3105f9dbee1de61342e
Philip Rideout <philiprideout@gmail.com>
2018-07-19 22:07:36 -0700

    Separate color pass.

--- commands/infinite_zoom.cc
+++ commands/infinite_zoom.cc
@@ -12,42 +12,10 @@ using namespace std;
 
 namespace {
 
-static constexpr int NoiseTableSize = 256;
-static const int NoiseTableMask = NoiseTableSize - 1;
-static int NoiseTablePerm[NoiseTableSize];
-static vec2 NoiseTableGrad[NoiseTableSize];
-
-void initnoise(uint32_t seed) {
-    mt19937 randomGenerator(seed);
-    iota(NoiseTablePerm, NoiseTablePerm + NoiseTableSize, 0);
-    shuffle(NoiseTablePerm, NoiseTablePerm + NoiseTableSize, randomGenerator);
-    for (int index = 0; index < NoiseTableMask; ++index) {
-        const float theta = 2.0f * pi<float>() * index / NoiseTableMask;
-        NoiseTableGrad[index] = {cosf(theta), sinf(theta)};
-    }
-}
-
-vec2 noisegrad(i32vec2 v) {
-    int i = v.x, j = v.y;
-    int hash = NoiseTablePerm[ ( NoiseTablePerm[ i & NoiseTableMask ] + j ) & NoiseTableMask ];
-    return NoiseTableGrad[hash];
-}
-
-vec4 vcolor(uint32_t hex) {
-    return {hex >> 16, (hex >> 8) & 0xff, hex & 0xff, 0xff};
-}
-
-vec3 gradnoise(vec2 p);
-float gridlines(vec2 p);
-
 struct PixelRay {
     u16vec2 origin;
     u16vec2 target;
 };
-
-float falloff(float t) {
-    return 1.0f - (3.0f - 2.0f * abs(t)) * t * t;
-}
 
 struct InfiniteZoom : ClumpyCommand {
     InfiniteZoom() {}
@@ -63,77 +31,37 @@ struct InfiniteZoom : ClumpyCommand {
     }
 };
 
-ClumpyCommand::Register registrar("infinite_zoom", [] {
+static ClumpyCommand::Register registrar("infinite_zoom", [] {
     return new InfiniteZoom();
 });
 
-bool InfiniteZoom::exec(vector<string> vargs) {
-    if (vargs.size() != 3) {
-        fmt::print("The command takes 3 arguments.\n");
-        return false;
+constexpr int NoiseTableSize = 256;
+constexpr int NoiseTableMask = NoiseTableSize - 1;
+int NoiseTablePerm[NoiseTableSize];
+vec2 NoiseTableGrad[NoiseTableSize];
+
+void initnoise(uint32_t seed) {
+    mt19937 randomGenerator(seed);
+    iota(NoiseTablePerm, NoiseTablePerm + NoiseTableSize, 0);
+    shuffle(NoiseTablePerm, NoiseTablePerm + NoiseTableSize, randomGenerator);
+    for (int index = 0; index < NoiseTableMask; ++index) {
+        float theta = 2.0f * pi<float>() * index / NoiseTableMask;
+        NoiseTableGrad[index] = {cosf(theta), sinf(theta)};
     }
-    string dims = vargs[0];
-    const uint32_t width = atoi(dims.c_str());
-    const uint32_t height = atoi(dims.substr(dims.find('x') + 1).c_str());
-    const uint32_t nframes = atoi(vargs[1].c_str());
-    const uint32_t seed = atoi(vargs[2].c_str());
-    const string output_suffix = fmt::format("island{:02}.npy", seed);
+}
 
-    // Initialize the table of gradient vectors.
-    initnoise(seed);
+vec2 noisegrad(i32vec2 v) {
+    int i = v.x, j = v.y;
+    int hash = NoiseTablePerm[ ( NoiseTablePerm[ i & NoiseTableMask ] + j ) & NoiseTableMask ];
+    return NoiseTableGrad[hash];
+}
 
-    // "Raster Space" is <ui16,ui16> with 0,0 at upper left.
-    // "Viewport Space" is <fp32,fp32> with -1,+1 at upper left.
-    // -1.0 is the left  edge of pixel (0)
-    // +1.0 is the right edge of pixel (w-1)
+vec4 vcolor(uint32_t hex) {
+    return {hex >> 16, (hex >> 8) & 0xff, hex & 0xff, 0xff};
+}
 
-    const float dx = 2.0f / width;
-    const float sx = -1.0f + dx * 0.5;
-    const float dy = 2.0f / height;
-    const float sy = 1.0f - dy * 0.5;
-
-    vector<uint32_t> result(width * height);
-    u8vec4* pdata = (u8vec4*) result.data();
-    for (int row = 0; row < height; ++row) {
-        const float y = sy - row * dy;
-        for (int col = 0; col < width; ++col, ++pdata) {
-            const float x = sx + col * dx;
-            const vec2 p(x, y);
-
-            float freq = 4;
-            float ampl = 0.7;
-            float N = 0;
-            float G = 0;
-            N += ampl * gradnoise(p * freq).x;
-            G += ampl * gridlines(p * freq);
-            freq *= 2; ampl /= 2;
-            N += ampl * gradnoise(p * freq).x;
-            G += ampl * gridlines(p * freq);
-            freq *= 2; ampl /= 2;
-            N += ampl * gradnoise(p * freq).x;
-            G += ampl * gridlines(p * freq);
-            freq *= 2; ampl /= 2;
-            N += ampl * gradnoise(p * freq).x;
-            G += ampl * gridlines(p * freq);
-            freq *= 2; ampl /= 2;
-            N += ampl * gradnoise(p * freq).x;
-            G += ampl * gridlines(p * freq);
-
-            const float F = falloff(x) * falloff(y);
-            const float H = F + N - 0.5f;
-            const float B = smoothstep(0.02f, 0.0f, abs(H));
-            vec4 color = H > 0 ? vcolor(0x547238) : vcolor(0x365978);
-            color = mix(color, vcolor(0xb8c289), B);
-            const vec4 gridcolor = H > 0 ? vcolor(0x000000) : vcolor(0xffffff);
-            color = mix(color, gridcolor, G * 0.25);
-
-            *pdata = u8vec4(color);
-        }
-    }
-
-    cnpy::npy_save(output_suffix, result.data(), {height, width}, "w");
-    fmt::print("\nWrote {:03}{} through {:03}{}.\n", 0, output_suffix, nframes - 1, output_suffix);
-    return true;
+float falloff(float t) {
+    return 1.0f - (3.0f - 2.0f * abs(t)) * t * t;
 }
 
 // Returns gradient noise in .x and its derivatives in .yz
@@ -166,11 +94,73 @@ vec3 gradnoise(vec2 p) {
     return vec3(v, derivatives);
 }
 
-float gridlines(vec2 p) {
-    return 0;
-    // vec2 f = abs(vec2(0.5) - fract(p));
-    // float m = std::max(f.x, f.y);
-    // return smoothstep(0.45f, 0.5f, m);
+bool InfiniteZoom::exec(vector<string> vargs) {
+    if (vargs.size() != 3) {
+        fmt::print("The command takes 3 arguments.\n");
+        return false;
+    }
+    string dims = vargs[0];
+    const uint32_t width = atoi(dims.c_str());
+    const uint32_t height = atoi(dims.substr(dims.find('x') + 1).c_str());
+    const uint32_t nframes = atoi(vargs[1].c_str());
+    const uint32_t seed = atoi(vargs[2].c_str());
+    const string output_suffix = fmt::format("island{:02}.npy", seed);
+
+    // Initialize the table of gradient vectors.
+    initnoise(seed);
+
+    // "Raster Space" is <ui16,ui16> with 0,0 at upper left.
+    // "Viewport Space" is <fp32,fp32> with -1,+1 at upper left.
+    // -1.0 is the left  edge of pixel (0)
+    // +1.0 is the right edge of pixel (w-1)
+
+    const float dx = 2.0f / width;
+    const float sx = -1.0f + dx * 0.5;
+    const float dy = 2.0f / height;
+    const float sy = 1.0f - dy * 0.5;
+
+    vector<float> fresult(width * height);
+    float* fdata = fresult.data();
+    for (int row = 0; row < height; ++row) {
+        const float y = sy - row * dy;
+        for (int col = 0; col < width; ++col, ++fdata) {
+            const float x = sx + col * dx;
+            const vec2 p(x, y);
+            float freq = 4;
+            float ampl = 0.7;
+            float N = 0;
+            N += ampl * gradnoise(p * freq).x;
+            freq *= 2; ampl /= 2;
+            N += ampl * gradnoise(p * freq).x;
+            freq *= 2; ampl /= 2;
+            N += ampl * gradnoise(p * freq).x;
+            freq *= 2; ampl /= 2;
+            N += ampl * gradnoise(p * freq).x;
+            freq *= 2; ampl /= 2;
+            N += ampl * gradnoise(p * freq).x;
+            const float F = falloff(x) * falloff(y);
+            *fdata = F + N - 0.5f;
+        }
+    }
+
+    vector<uint32_t> iresult(width * height);
+    u8vec4* idata = (u8vec4*) iresult.data();
+    fdata = fresult.data();
+    for (int row = 0; row < height; ++row) {
+        for (int col = 0; col < width; ++col, ++idata, ++fdata) {
+            const float H = *fdata;
+            const float B = smoothstep(0.02f, 0.0f, abs(H));
+            vec4 color = H > 0 ? vcolor(0x547238) : vcolor(0x365978);
+            *idata = mix(color, vcolor(0xb8c289), B);
+        }
+    }
+
+    fresult.clear();
+    fresult.shrink_to_fit();
+
+    cnpy::npy_save(output_suffix, iresult.data(), {height, width}, "w");
+    fmt::print("\nWrote {:03}{} through {:03}{}.\n", 0, output_suffix, nframes - 1, output_suffix);
+    return true;
 }
 
 }
