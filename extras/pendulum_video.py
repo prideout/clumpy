2b9d69336fa84bc95403652157981c36fb27722d
Philip Rideout <philiprideout@gmail.com>
2019-06-17 18:51:33 -0700

    Fix up pendulum video script.

--- extras/pendulum_video.py
+++ extras/pendulum_video.py
@@ -14,10 +14,10 @@ def clumpy(cmd):
 spacing = 20
 step_size = 2.5
 skip = 2
-kernel_size = 5
+kernel_size = 7
 decay = 0.99
-nframes = 240
-res = 2000, 2000
+nframes = 240 * 4
+res = 2000*2, 2000*2
 dim = 'x'.join(map(str,res))
 
 friction = 0.1
@@ -27,7 +27,7 @@ clumpy('advect_points pts.npy field.npy ' +
     f'{step_size} {kernel_size} {decay} {nframes} anim1.npy')
 
 friction = 0.9
-clumpy(f'pendulum_phase field {dim} {friction} field.npy')
+clumpy(f'pendulum_phase {dim} {friction} 1 5 field.npy')
 clumpy(f'bridson_points {dim} {spacing} 0 pts.npy')
 clumpy('advect_points pts.npy field.npy ' +
     f'{step_size} {kernel_size} {decay} {nframes} anim2.npy')
@@ -37,10 +37,10 @@ writer = imageio.get_writer('anim.mp4', fps=60)
 for i in tqdm(range(0, nframes, skip)):
 
     im1 = snowy.reshape(np.load("{:03}anim1.npy".format(i)))
-    im1 = snowy.resize(im1, 256, 256)
+    im1 = snowy.resize(im1, 960-6, 1088-8)
 
     im2 = snowy.reshape(np.load("{:03}anim2.npy".format(i)))
-    im2 = snowy.resize(im2, 256, 256)
+    im2 = snowy.resize(im2, 960-6, 1088-8)
 
     im = np.uint8(255.0 - snowy.hstack([im1, im2], border_width=4))
     writer.append_data(im)
